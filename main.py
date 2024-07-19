c4b3d59b140c4a9c20573be298c0071299b6315a
Kyle B. Lackinger <karl.B.HODL@proton.me>
2024-07-19 00:29:13 -0400

    Added XMP embedding capability

--- main.py
+++ main.py
@@ -15,8 +15,13 @@ new_img = Picture(public_key, private_key, file_path, new_file_path)
 new_img.add_metadata()
 
 # Extract the metadata
-extracted_hash = new_img.extract_metadata()
+metadata = new_img.extract_metadata()
+extracted_exif_hash = metadata['exif_hash']
+extracted_xmp_hash = metadata['xmp_hash']
 
 # Verify extracted data matches the hash of the secret key
 hashed_secret_key = hashlib.sha256(private_key.encode("utf-8")).hexdigest().encode("utf-8")
-print(f'Hashes match: {hashed_secret_key == extracted_hash}')
+print(f'EXIF hash matches: {hashed_secret_key == extracted_exif_hash}')
+print(f'XMP hash matches: {hashed_secret_key == extracted_xmp_hash}')
+print(f'Extracted XMP hash: {extracted_xmp_hash}')
+print(f'Hash: {hashed_secret_key}')
--- picture.py
+++ picture.py
@@ -1,7 +1,15 @@
 """A class that can represent a jpeg picture."""
+import traceback
 from PIL import Image
 from PIL.ExifTags import TAGS
 from utils import encode_key, hash_metadata
+from libxmp import XMPFiles, XMPMeta, XMPError
+
+# Define the namespace URI
+NS_CUSTOM = 'http://ns.example.com/custom/1.0/'
+
+# Register the custom namespace with a prefix
+XMPMeta.register_namespace(NS_CUSTOM, 'custom')
 
 class Picture:
     """A representation of a picture"""
@@ -15,7 +23,7 @@ class Picture:
 
     def add_metadata(self):
         """
-        Add metadata to an image file and save the file
+        Add EXIF and XMP metadata to an image file and save the file
 
         Parameters:
         public_key (string) - public key to embed into image
@@ -40,18 +48,55 @@ class Picture:
         # Add the exif data to the image
         img.save(self.new_img_path, exif=exif_data)
 
+        # Add xmp metatdata
+        xmp_file = XMPFiles(file_path=self.new_img_path, open_forupdate=True)
+        xmp = xmp_file.get_xmp()
+        if xmp is None:
+            xmp = XMPMeta()
+        
+        try:
+            xmp.set_property(NS_CUSTOM, 'public_key', self.pub_key)
+            xmp.set_property(NS_CUSTOM, 'hashed_private_key', hashed_private_key.encode('utf-8').hex())
+            if xmp_file.can_put_xmp(xmp):
+                xmp_file.put_xmp(xmp)
+            xmp_file.close_file()
+        except XMPError as e:
+            print(f'An error occurred while writing XMP Metadata: {str(e)}.')
+
     def extract_metadata(self):
         """
         Extract the added metadata to read it back
         """
+        exif_hash = None
+        xmp_hash = None
+
+        # Extract EXIF
         img = Image.open(self.new_img_path)
         exif = img.getexif()
-        hash = None
 
         if exif:
             for tag_id, value in exif.items():
                 tag = TAGS.get(tag_id, tag_id)
                 if tag_id == 35701:
-                    hash = value
+                    exif_hash = value
                 # print(f"{tag}: {value}")  
-        return hash  
+        
+        # Extract XMP
+        xmp_file = XMPFiles(file_path=self.new_img_path)
+        xmp = xmp_file.get_xmp()
+        if xmp is not None:
+            try:
+                xmp_hash_hex = xmp.get_property(NS_CUSTOM, 'hashed_private_key')
+                if xmp_hash_hex:
+                    xmp_hash = bytes.fromhex(xmp_hash_hex)
+            except XMPError as e:
+                print(f'An error occurred while reading XMP metadata: {str(e)}')
+                print(f'Error type: {type(e).__name__}')
+                print(f'Error details:')
+                traceback.print_exc()
+        xmp_file.close_file()
+        
+        return {
+            'exif_hash': exif_hash,
+            'xmp_hash': xmp_hash
+        }  
